4e7bad1a37a5ae0de563e9905e1dcbe9e9c3c18d
Mon MacBook <justine.fernandez1990@gmail.com>
2023-05-16 19:41:24 +0200

    end of first version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # morpion
 mini projet jeu du pendu
+
+Le but de ce jeu est de deviner un mot défini de façon aléatoire avec une nombre de vie.
+
+Ce programme ne traite pas la gestion d'erreur, on part du principe que l'utilisateur renseigne uniquement des caractères valides, à savoir des lettres sans caractères avec accent.
---
+++ index.py
@@ -0,0 +1,31 @@
+import random
+
+words = "manger courir belle mariage pestiferer mordre redondance site internet jouer"
+words_list = words.split()
+secret = random.randint(0, len(words_list) - 1)
+secret_word = words_list[secret]
+game = {
+  "secret_word" : secret_word,
+  "guess_word" : "_" * len(secret_word),
+  "life" : 9
+}
+
+print(f'{ game["guess_word"] } | vie : { game["life"] }')
+
+while True:
+  letter = input('Choisissez une lettre >')
+  if letter in game["secret_word"] and letter not in game["guess_word"]:
+    guess_word_list = list(game['guess_word'])
+    for index, current_letter in enumerate (game['secret_word']):
+      if current_letter == letter:
+        guess_word_list[index] = letter
+    game["guess_word"] = "".join(guess_word_list)
+  elif letter not in game["secret_word"]:
+    game['life'] -= 1
+  print(f'{ game["guess_word"] } | vie : { game["life"] }')
+  if "_" not in game["guess_word"]:
+    print('Vous avez gagné')
+    break
+  elif game['life'] < 1:
+    print('Vous avez perdu')
+    break
